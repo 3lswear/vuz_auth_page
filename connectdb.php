fb92709711b460bb747610e886e3eaf5b19a3f8d
3lswear <r.malyavsky@protonmail.com>
2020-12-14 14:58:50 +0300

    modified user to access db

--- connectdb.php
+++ connectdb.php
@@ -1,8 +1,8 @@
 <?php
 //данные о хосте, пользователе и базе данных
 $host = 'localhost';
-$user = 'root';
-$pass = '';
+$user = 'user_for_auth_page';
+$pass = 'Oppressed-Unwilling-Sloped4';
 $dbname = 'auth_page';
 // подключаемся и выбираем бд, которую указали выше
 $link = mysqli_connect($host, $user, $pass, $dbname);
